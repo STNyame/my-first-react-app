2e403e96338caeb867ade9b823b945655eee6652
STNyame <sentwalidors@hotmail.com>
2021-02-17 17:42:45 +0100

    random

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import title from "./components/Title.js";
 
 function App() {
   return (
     <div className="App">
+      <Title />
       <p>Welcome codaisseur</p>
     </div>
   );
